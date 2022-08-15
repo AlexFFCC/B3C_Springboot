de52ab84289be243e1056828c5bb93cb54c8e209
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2022-08-15 20:36:05 +0800

    init

---
+++ src/main/java/com/alex/buy3c/Buy3cSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.alex.buy3c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buy3cSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buy3cSpringboot1Application.class, args);
+	}
+
+}
