587a58e9586aece5da7012fef9077743697d4e1c
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2023-01-16 21:09:32 +0800

    add test branch line

--- src/main/java/com/alex/buy3c/TestMain.java
+++ src/main/java/com/alex/buy3c/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("add print line");
 		System.out.println("two line");
+		System.out.println("test branch");
 
 	}
 
