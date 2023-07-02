fff710cd67888fb1a1edc5148c90316ffa5e768c
AlexFFCC <m3zo0926@gmail.com>
2023-07-02 21:15:27 +0800

    git m3zo0926 account commit

--- src/main/java/com/alex/buy3c/TestMain.java
+++ src/main/java/com/alex/buy3c/TestMain.java
@@ -10,6 +10,7 @@ public class TestMain {
 		System.out.println("test git user2");
 		System.out.println("test git user3");
 		System.out.println("update token");
+		System.out.println("git m3zo0926 account commit");
 
 	}
 
